73998480d196bb52cbcb33bba5414beb5b8e266b
sugarycoke <58714552+sugarycoke@users.noreply.github.com>
2020-09-19 18:29:16 -0400

    added calls, changed format to json

--- src/nodes/notifications.py
+++ src/nodes/notifications.py
@@ -1,15 +1,37 @@
 import twilio.rest
 import os
+import json
 
 accountsid = "AC9e4a91e0e6865a7d8c5534766f11d532"
 authtoken = "05d27a08354de3223e2b9baef869ce43"
 client = twilio.rest.Client(accountsid, authtoken)
 
+cases = [
+    "https://handler.twilio.com/twiml/EHcc17118fc21ef15b711bf26d5329777b", # "Hello, test case."
+    "https://handler.twilio.com/twiml/EH37749524b98862ba08a75c2370b1a9f9", # Armed person case
+    "https://handler.twilio.com/twiml/EHa9ac8807f1c3f122d959e5b521de2414", # Unknown person case
+    "https://handler.twilio.com/twiml/EH034b64c120e69bfcd544f0c572c9d359", # Package delivery case
+    ""
+    
+]
+
 os.chdir('../')
-with open('assets/numbers.txt', 'r') as f:
-    numbers = f.readlines()
+with open('assets/numbers.json') as f:
+    numbers = json.load(f)
+
+print(numbers['Numbers'])
+numbers = numbers['Numbers']
+
 
 def sendMsg(msg):
     global numbers
     for number in numbers:
+        print(number)
         client.messages.create(body=msg, from_="+13017195667", to=number.strip('\n'))
+
+def makeCall(num):
+    global numbers
+    for number in numbers:
+        client.calls.create(to=number, from_="+13017195667", url=cases[num])
+
+
