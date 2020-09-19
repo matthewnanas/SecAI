69a8df93e286f203d3b5cab83a3ec2cacf7076e3
sugarycoke <58714552+sugarycoke@users.noreply.github.com>
2020-09-19 02:06:31 -0400

    Updated notifications, pulls from new text file numbers.txt from src/assets, added more images.

--- src/nodes/notifications.py
+++ src/nodes/notifications.py
@@ -1,10 +1,15 @@
 import twilio.rest
+import os
 
 accountsid = "AC9e4a91e0e6865a7d8c5534766f11d532"
 authtoken = "05d27a08354de3223e2b9baef869ce43"
 client = twilio.rest.Client(accountsid, authtoken)
 
+os.chdir('../')
+with open('assets/numbers.txt', 'r') as f:
+    numbers = f.readlines()
 
-def sendMsg(msg, numbers):
-    for x in numbers:
-        client.messages.create(body=msg, from_="+13017195667", to=x)
+def sendMsg(msg):
+    global numbers
+    for number in numbers:
+        client.messages.create(body=msg, from_="+13017195667", to=number.strip('\n'))
